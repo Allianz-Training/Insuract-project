9a02ca13228ca219f33fc787daeef2ae7e82e681
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2020-12-26 16:18:31 +0700

    finish login

--- src/app/default/default.module.ts
+++ src/app/default/default.module.ts
@@ -8,6 +8,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [DefaultComponent, LoginComponent, RegisterComponent],
@@ -17,6 +19,9 @@ import { RegisterComponent } from './register/register.component';
     SharedModule,
     MaterialModule,
     FlexLayoutModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
   ],
   exports: [DefaultComponent],
 })
--- src/app/default/login/login.component.ts
+++ src/app/default/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginInfo: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.loginInfo = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  onLogin() {
+    console.warn(this.loginInfo.value);
+  }
 }
