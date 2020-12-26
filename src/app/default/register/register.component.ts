5f7100d082ca32d1eab4d8028e7be009a3143f38
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2020-12-26 18:24:04 +0700

    working on confirm password

--- src/app/default/register/register.component.ts
+++ src/app/default/register/register.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  accountForm: FormGroup;
+  personalForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.accountForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmpassword: ['', [Validators.required]],
+    });
 
-  ngOnInit(): void {
+    this.personalForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      citizenID: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(13),
+          Validators.maxLength(13),
+        ],
+      ],
+      telnum: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
 }
