8fd1d8ae74ab2fe826593f8bff09a1ba64739d8c
time <jirayut_time@hotmail.com>
2020-12-31 19:46:25 +0700

    register method

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,17 @@ export class UsersService {
 
   constructor(private http:HttpClient) { }
 
+  public register(user: any){
+    return this.http.post(`${this.baseUrl}auth/register`,user,{observe: 'response'})
+  }
+
   public validateUser(user:string){
     return this.http.post(`${this.baseUrl}auth/validate-user`,{"username":user},{observe : 'response'}).toPromise()
   }
   public validateEmail(email:string){
-    return this.http.post(`${this.baseUrl}auth/validate-email`,{"username":email},{observe : 'response'}).toPromise()
+    return this.http.post(`${this.baseUrl}auth/validate-email`,{"email":email},{observe : 'response'}).toPromise()
   }
   public validateCitizenId(citizenId:string){
-    return this.http.post(`${this.baseUrl}auth/validate-citizen-id`,{"username":citizenId},{observe : 'response'}).toPromise()
+    return this.http.post(`${this.baseUrl}auth/validate-citizen-id`,{"citizenId":citizenId},{observe : 'response'}).toPromise()
   }
 }
